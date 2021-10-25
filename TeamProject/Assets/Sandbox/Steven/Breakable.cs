7528a3626386d7758544cd752e40eaf3c84eb572
Steven <ste77236@gmail.com>
2021-10-24 20:55:17 -0400

    Added code to detect when object should break

---
+++ TeamProject/Assets/Sandbox/Steven/Breakable.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour
+{
+    [SerializeField] private float breakForce = 8.0f;
+
+    [SerializeField] private GameObject brokenModel;
+
+    void Break()
+    {
+        Debug.Log("BROKEN");
+        
+        // TODO Instantiate your broken model here and then delete this gameobject
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Solid"))
+        {
+            Vector3 impactForce = other.relativeVelocity;
+            
+            Debug.Log("Force = " + other.relativeVelocity);
+            
+            if (impactForce.x >= breakForce || impactForce.y >= breakForce || impactForce.z >= breakForce)
+                Break();
+        }
+    }
+}
