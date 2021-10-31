51a15c4260998439889cb382a28717815391b4d2
xknoll56 <xavierknoll56@gmail.com>
2021-10-30 21:21:28 -0400

    more aimless movement pattern

--- TeamProject/Assets/Sandbox/Xavier/FlyNavigator.cs
+++ TeamProject/Assets/Sandbox/Xavier/FlyNavigator.cs
@@ -3,9 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+
+public enum AgentMode
+{
+    TARGET,
+    AIMLESS
+}
+
 [RequireComponent(typeof(NavMeshAgent))]
 public class FlyNavigator : MonoBehaviour
 {
+    [SerializeField]
+    private float wallAwarenessDistance;
+
     private NavMeshAgent agent;
     public GameObject destinationsContainer;
     private BoxCollider[] destinationVolumes;
@@ -13,6 +23,12 @@ public class FlyNavigator : MonoBehaviour
     private Vector3 destination;
     private FlyRelativeMovmement flyRelativeMovement;
     private bool newDestinationAssigned;
+    private AgentMode mode;
+    private bool aimlessTargetSet;
+    private Vector3 targetDestination;
+
+    float aimlessTargetTimer = 0.0f;
+    float aimlessTargetCooldown = 5.0f;
 
     void Start()
     {
@@ -21,6 +37,9 @@ public class FlyNavigator : MonoBehaviour
         destinationVolumes = destinationsContainer.GetComponentsInChildren<BoxCollider>();
         currentTargetIndex = 0;
         GetNewDestination(currentTargetIndex);
+        mode = AgentMode.AIMLESS;
+        aimlessTargetSet = false;
+        agent.stoppingDistance = 3.0f;
     }
 
     public void GetNewDestination()
@@ -28,7 +47,6 @@ public class FlyNavigator : MonoBehaviour
         int newTargetIndex = (int)Random.Range(0.0f, destinationVolumes.Length);
         if (newTargetIndex == currentTargetIndex)
         {
-            print("new destination same as last destination");
             if (currentTargetIndex == destinationVolumes.Length - 1)
                 currentTargetIndex = 0;
             else
@@ -45,23 +63,72 @@ public class FlyNavigator : MonoBehaviour
         Vector3 dest = currentCollider.bounds.center
             + new Vector3(Random.Range(-currentCollider.bounds.size.x / 2, currentCollider.bounds.size.x / 2)
             , 0, Random.Range(-currentCollider.bounds.size.z / 2, currentCollider.bounds.size.z / 2));
+        targetDestination = dest;
         agent.destination = dest;
         newDestinationAssigned = true;
         
     }
 
+    private void MoveAimlesssly()
+    {
+
+    }
 
     void Update()
     {
-        if (!newDestinationAssigned)
+        switch(mode)
         {
-            if (flyRelativeMovement.GetFlyMode() == FlyMode.TRAVELLING)
-                if (agent.remainingDistance < 0.1f)
+            //case AgentMode.TARGET:
+            //    aimlessTargetTimer += Time.deltaTime;
+            //    if(aimlessTargetTimer>aimlessTargetCooldown)
+            //    {
+            //        aimlessTargetTimer = 0.0f;
+            //        mode = AgentMode.AIMLESS;
+            //        return;
+            //    }
+            //    if (!newDestinationAssigned)
+            //    {
+            //        if (flyRelativeMovement.GetFlyMode() == FlyMode.TRAVELLING)
+            //            if (agent.remainingDistance < 0.1f)
+            //            {
+            //                flyRelativeMovement.SetFlyMode(FlyMode.FLOOR_LANDING);
+            //            }
+            //    }
+            //    else
+            //        newDestinationAssigned = false;
+            //    break;
+            case AgentMode.AIMLESS:
+                if (!aimlessTargetSet)
                 {
-                    flyRelativeMovement.SetFlyMode(FlyMode.FLOOR_LANDING);
+                    RaycastHit hit, hitRight, hitLeft;
+                    Physics.Raycast(transform.position, transform.right, out hitRight);
+                    Physics.Raycast(transform.position, -transform.right, out hitLeft);
+                    Physics.Raycast(transform.position, transform.forward, out hit);
+                    if(hit.distance>hitRight.distance && hit.distance>hitLeft.distance)
+                    {
+                        agent.destination = hit.point - transform.forward * wallAwarenessDistance;
+                        aimlessTargetSet = true;
+                    }
+                    else if(hitLeft.distance>hitRight.distance)
+                    {
+                        agent.destination = hitLeft.point + transform.right * wallAwarenessDistance;
+                        aimlessTargetSet = true;
+                    }
+                    else
+                    {
+                        agent.destination = hitRight.point - transform.right * wallAwarenessDistance;
+                        aimlessTargetSet = true;
+                    }
+                       // mode = AgentMode.TARGET;
                 }
+                if (agent.remainingDistance < agent.stoppingDistance)
+                {
+                    //agent.destination = targetDestination;
+                    //mode = AgentMode.TARGET;
+                    aimlessTargetSet = false;
+                }
+                break;
         }
-        else
-            newDestinationAssigned = false;
+
     }
 }
