57ec676256c7f824df752c48421b382912aa7a02
Steven <ste77236@gmail.com>
2021-11-13 15:26:03 -0500

    Added fly counter, pause menu, game timer

--- TeamProject/Assets/Sandbox/Steven/LightHandler.cs
+++ TeamProject/Assets/Sandbox/Steven/LightHandler.cs
@@ -6,27 +6,24 @@ using UnityEngine;
 public class LightHandler : MonoBehaviour
 {
     [SerializeField] private List<GameObject> lightSources;
+    
     private AudioSource _toggleSound;
 
     private void Awake()
     {
         _toggleSound = GetComponent<AudioSource>();
-    }
-
-    void Start()
-    {
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Turn on every light on awake to avoid potential problems
+        foreach (var lightSource in lightSources)
+        {
+            lightSource.SetActive(true);
+        }
     }
 
     public void ToggleLight()
     {
         _toggleSound.Play();
+        
         foreach (var lightSource in lightSources)
         {
             lightSource.SetActive(!lightSource.activeSelf);
