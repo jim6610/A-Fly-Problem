091ebb32789e9e0665fd52e343dad39c01d7ee63
Jimmy Wong <jimmy-6610@hotmail.com>
2021-11-18 02:41:54 -0500

    Destructible objects update
    - Removed additional useless files
    - Added ability to toggle destroy object after specified amount of time
    - added Steven's lights to prototype scene

--- TeamProject/Assets/Scripts/Destructible/Destructible.cs
+++ TeamProject/Assets/Scripts/Destructible/Destructible.cs
@@ -1,3 +1,7 @@
+// Part of the script is from the Destructible Props Pack by Vitaly
+// https://assetstore.unity.com/packages/3d/props/destructible-props-pack-27379
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +10,13 @@ public class Destructible : MonoBehaviour {
     [SerializeField] private float initialHealth;
     [SerializeField] private float monetaryValue;
     [SerializeField] private GameObject[] destroyedPieces;
-    [SerializeField] private GameObject[] hiddenPieces; // list of the objects that will be hidden after the object breaks
+    [SerializeField] private GameObject[] hidingPieces; // list of the objects that will be hidden after the object breaks
     [SerializeField] private float explosionForce = 200; // force added to every piece of the broken object
     [SerializeField] private float destroyedPiecesRotation = 20; // Rotation force added to every chunk when it explodes
     [SerializeField] private float breakForce; // How easily the object breaks
-    [SerializeField] private GameObject FX;
+    [SerializeField] private GameObject FX; // special effect
+    [SerializeField] private bool destroyAftertime = true; // if true, then destroyed pieces will be removed after a set time
+    [SerializeField] private float time = 5; // time before destroyed pieces will be destroyed from the scene
 
     private float currentHealth;
     private float currentValue;
@@ -83,9 +89,10 @@ public class Destructible : MonoBehaviour {
     {
         LoseRemainingValue();
 
-        if (hiddenPieces.Length != 0)
+        // if there were any 
+        if (hidingPieces.Length != 0)
         {
-            foreach (GameObject hidingObj in hiddenPieces)
+            foreach (GameObject hidingObj in hidingPieces)
             {
                 hidingObj.SetActive(false);
             }
@@ -108,7 +115,16 @@ public class Destructible : MonoBehaviour {
             chunk.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * -explosionForce);
             chunk.GetComponent<Rigidbody>().AddRelativeTorque(Vector3.forward * -destroyedPiecesRotation * Random.Range(-5f, 5f));
             chunk.GetComponent<Rigidbody>().AddRelativeTorque(Vector3.right * -destroyedPiecesRotation * Random.Range(-5f, 5f));
+            if (destroyAftertime)
+            {
+                Invoke("DestructObject", time);
+            }
         }
+    }
+
+    void DestructObject()
+    {
+        Destroy(gameObject);
     }
 
     private void OnCollisionEnter(Collision other)
