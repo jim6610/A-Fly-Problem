dea1e837bbec8f4dd7548d03b47b4656bbac6ca4
xknoll56 <xavierknoll56@gmail.com>
2021-11-24 22:47:10 -0500

    bringing web over

---
+++ TeamProject/Assets/Sandbox/Xavier/WebEffect.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebEffect : MonoBehaviour
+{
+
+    public float debuffDuration = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(selfDestroy());
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        PlayerMovement movement = GetComponent<PlayerMovement>();
+        if(movement)
+        {
+            /*Debug.Log("web stepped on");*/
+            movement.StartMovementSlowdown(debuffDuration);
+            Destroy(this.gameObject);
+        }                
+    }
+
+    private IEnumerator selfDestroy()
+    {
+        float timeUntilDestruction = 10f; //web will be detroyed automatically 10 seconds after spawning
+
+        while (timeUntilDestruction >= 0f)
+        {
+            /*Debug.Log(timeUntilDestruction);*/
+            yield return new WaitForSeconds(1);
+            timeUntilDestruction -= 1f;
+        }
+
+        Destroy(this.gameObject);
+    }
+}
